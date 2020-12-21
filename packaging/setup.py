94d191408c55e13d3b28a4c800aabda951afec39
Rich Dobbs <rich.dobbs.13440@gmail.com>
2020-12-20 19:18:47 -0700

    Get static data into package

--- packaging/setup.py
+++ packaging/setup.py
@@ -3,5 +3,13 @@ import setuptools
 
 setuptools.setup(
     package_dir={'': "../src/"},
-    packages=["astutus"],
+    # packages=setuptools.find_packages(),
+    packages=['astutus', 'astutus.web'],
+    package_data={
+        'astutus': [
+            'web/static/**/*',
+            'web/static/_docs/_static/*',
+            'web/static/_docs/_static/**/*'
+        ]
+    }
 )
