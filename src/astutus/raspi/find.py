ba17762afb92a07ab400da0739bd53c2f2db4520
Rich Dobbs <rich.dobbs.13440@gmail.com>
2020-12-22 05:02:08 -0700

    Searching, breaking into sections, basic filters

--- src/astutus/raspi/find.py
+++ src/astutus/raspi/find.py
@@ -1,9 +1,29 @@
 import subprocess
 import logging
+import re
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+def parse_section_to_dictionary(section: str) -> {}:
+    # Nmap scan report for 192.168.0.65
+    # Host is up (0.00022s latency).
+    # Not shown: 998 closed ports
+    # PORT     STATE SERVICE
+    # 22/tcp   open  ssh
+    # 5900/tcp open  vnc
+    # MAC Address: DC:A6:32:C5:94:AD (Raspberry Pi Trading)
+    pattern = r'((\d{1,3}\.){3}\d{1,3})'
+    matches = re.search(pattern, section)
+    ipv4 = matches.group(0)
+    logger.debug(f"ipv4: {ipv4}")
+    parsed_section = {
+        'ipv4': ipv4,
+        'content': section,
+    }
+    return parsed_section
 
 
 def search_using_nmap(ipv4: str, mask: int, filter: [str]):
@@ -18,6 +38,6 @@ def search_using_nmap(ipv4: str, mask: int, filter: [str]):
     for section in sections:
         for item in filter:
             if item in section:
-                results.append(section)
+                results.append(parse_section_to_dictionary(section))
                 break
     return results
--- src/astutus/web/flask_app.py
+++ src/astutus/web/flask_app.py
@@ -14,17 +14,11 @@ logger.setLevel(logging.DEBUG)
 
 app = flask.Flask('astutus.web.flask_app', template_folder='templates')
 
-library_handler = logging.StreamHandler(flask.logging.wsgi_errors_stream)
-library_handler.setFormatter(
-    # Set format suitable for clicking in terminal in Visual Studio Code (Ctrl or Cmd Click)
-    logging.Formatter("[%(asctime)s] %(levelname)s File \"%(pathname)s:%(lineno)d\" %(message)s")
-)
 flask.logging.default_handler.setFormatter(
     logging.Formatter("[%(asctime)s] %(levelname)s File \"%(pathname)s:%(lineno)d\" %(message)s")
 )
-# app.logger.addHandler(flask.logging.default_handler)
-astutus.raspi.find.logger.addHandler(library_handler)
-logger.addHandler(library_handler)
+astutus.raspi.find.logger.addHandler(flask.logging.default_handler)
+logger.addHandler(flask.logging.default_handler)
 
 
 @app.template_filter('tojson_pretty')
@@ -60,29 +54,17 @@ def handle_rasp_find(form):
     logger.debug(f"ipv4: {ipv4}")
     mask = flask.request.form.get("mask")
     logger.debug(f"mask: {mask}")
-    filter = []
-    raspi_filter = form.get("raspi")
-    logger.debug(f"raspi_filter: {raspi_filter}")
-    if raspi_filter is not None:
-        filter.append(raspi_filter)
-    vnc_filter = form.get("vnc")
-    logger.debug(f"vnc_filter: {vnc_filter}")
-    if vnc_filter is not None:
-        filter.append(vnc_filter)
-    ssh_filter = form.get("ssh")
-    logger.debug(f"ssh_filter: {ssh_filter}")
-    if ssh_filter is not None:
-        filter.append(ssh_filter)
+    filter = form.getlist("filter")
+    logger.debug(f"filter: {filter}")
     search_result = astutus.raspi.search_using_nmap(ipv4, mask, filter)
-    return flask.render_template('raspi_find.html', search_result=search_result)
-
+    return flask.render_template('raspi_find.html', search_result=search_result, filter=filter)
 
 
 @app.route('/astutus/raspi', methods=['POST', 'GET'])
 def handle_raspi():
     if flask.request.method == 'GET':
         if flask.request.args.get('find') is not None:
-            return flask.render_template('raspi_find.html', search_result=None)
+            return flask.render_template('raspi_find.html', search_result=None, filter=['Raspberry'])
         page_data = {
             'title': "Astutus/Raspberry Pi's",
             'show_links_section': True,
