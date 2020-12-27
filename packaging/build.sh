c10c9250a5b0313966ab02fc29bc9a863da2a9c2
Rich Dobbs <rich.dobbs.13440@gmail.com>
2020-12-27 14:39:32 -0700

    Make venv if necessary during build

--- packaging/build.sh
+++ packaging/build.sh
@@ -7,6 +7,10 @@ this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${this_dir}/utilities.sh"
 
 mark_section "Start Building"
+
+if [ ! -d "${REPOPATH}/venv/bin/" ]; then
+    source "${this_dir}/make_venv.sh"
+fi
 export REPOPATH=$( cd ${this_dir}/.. ; pwd )
 cp "${this_dir}/astutus_customization.sh" "${REPOPATH}/venv/bin/"
 
