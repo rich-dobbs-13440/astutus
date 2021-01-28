f9ab2cb54f55a9fd7cb03e9c7f8a989f90b26a44
Rich Dobbs <rich.dobbs.13440@gmail.com>
2021-01-27 18:57:02 -0700

    Trying to get pytests to run cleanly on GitHub

--- pytests/test_raspi.py
+++ pytests/test_raspi.py
@@ -5,6 +5,7 @@ import astutus.raspi
 logger = logging.getLogger(__name__)
 
 
+@pytest.mark.skip(reason="requires a RaspberryPi")
 def test_publish_wheels():
     raspi = astutus.raspi.RaspberryPi(ipv4='192.168.0.65')
     raspi.publish_wheels()
